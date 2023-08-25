54cc7fd16f737a5d08a70f1d66387e2f58f4da68
Eric Phifer <ericoncode@gmail.com>
2023-08-25 10:47:03 -0600

    moved contact form from component to html in indes.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import Seo from '../components/Seo'
 // Hero
 import About from '../components/About'
-import Contact from '../components/Contact'
 import EventsNP from '../components/EventsNP'
 import Hero from '../components/Hero'
 import Location from '../components/Location'
@@ -15,6 +14,9 @@ import SectionWithLink from '../components/SectionWithLink'
 import Sections from '../components/Sections'
 import Social from '../components/Social'
 import Testimonials from '../components/Testimonials'
+// Contact form
+import useContact from '../utils/useContact'
+import useForm from '../utils/useForm'
 
 const HeroStyles = styled.div`
   height: 100vh;
@@ -95,7 +97,120 @@ const HR = styled.hr`
   border: 0.5rem solid var(--orange);
 `
 
+const Form = styled.form`
+  max-width: 80rem;
+  width: 100%;
+  margin: 2rem auto;
+  transition: 0.5s all ease;
+  display: flex;
+  flex-flow: column nowrap;
+  label {
+    display: none;
+  }
+  input,
+  textarea {
+    font-size: 1.75rem;
+  }
+  legend {
+    width: 98%;
+    font-size: 5rem;
+    text-align: center;
+    margin-bottom: 3rem;
+  }
+  button {
+    align-self: end;
+  }
+`
+
+const InlineField = styled.fieldset`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(auto, 1fr));
+  gap: 0.5rem;
+  border: none;
+  margin-bottom: 2rem;
+  input {
+    padding: 1rem;
+    margin: 0 0.5rem;
+    border: none;
+    border-bottom: 0.2rem inset var(--gray);
+    background: transparent;
+  }
+  @media only screen and (max-width: 900px) {
+    display: flex;
+    flex-flow: column nowrap;
+  }
+  @media only screen and (max-height: 750px) {
+    margin-bottom: 1rem;
+    input {
+      font-size: 1.25rem;
+    }
+    a,
+    button {
+      font-size: 1.5rem;
+      margin-bottom: 1rem;
+    }
+  }
+`
+
+const FullField = styled.fieldset`
+  display: flex;
+  justify-content: center;
+  border: none;
+  input {
+    width: 100%;
+    padding: 1rem;
+    margin: 0 0.5rem;
+    border: none;
+    border-bottom: 0.2rem inset var(--gray);
+    background: transparent;
+  }
+  textarea {
+    width: 100%;
+    margin-left: 0.5rem;
+    padding: 1rem;
+    border: none;
+    border-bottom: 0.2rem inset var(--gray);
+    background: transparent;
+  }
+`
+
+const Submit = styled.button`
+  width: 20rem;
+  margin-bottom: 5rem;
+  padding: 1.5rem;
+  background: var(--blue);
+  color: var(--white);
+  border-radius: 1rem;
+  font-size: 1.85rem;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--white);
+    border: 0.2rem solid var(--blue);
+    color: var(--blue);
+  }
+  &:focus {
+    border: 0.2rem dotted var(--white);
+  }
+  @media only screen and (max-width: 900px) {
+    margin-bottom: 2rem;
+  }
+`
+
 export default function IndexPage() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  })
+  const { contact, error, loading, errMessage, submitContact } = useContact({
+    values,
+  })
+  console.log(contact, error, loading, submitContact)
+  if (errMessage) {
+    return <p>{errMessage}</p>
+  }
   return (
     <>
       <HeroStyles id="top">
@@ -120,7 +235,64 @@ export default function IndexPage() {
           <WhiteSpace>
             <H2 id="contact">Contact Us</H2>
             <HR />
-            <Contact />
+            <Form
+              method="post"
+              netlify-honeypot="bot-field"
+              data-netlify="true"
+              name="standard-contact"
+            >
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+              <FullField className="nameEmail">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={values.name}
+                  onChange={updateValue}
+                  placeholder="Name"
+                  className="required"
+                />
+              </FullField>
+              <InlineField className="emailPhone">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={updateValue}
+                  placeholder="Email"
+                  className="required"
+                />
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  value={values.phone}
+                  onChange={updateValue}
+                  placeholder="Phone"
+                  className="required"
+                />
+              </InlineField>
+              <FullField className="message">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  value={values.message}
+                  onChange={updateValue}
+                  rows="7"
+                  placeholder="What is your #1 need?"
+                  className="required"
+                />
+              </FullField>
+              <Submit type="submit" value="Submit">
+                Submit
+              </Submit>
+            </Form>
           </WhiteSpace>
         </LightBlock>
         <DarkBlock>
